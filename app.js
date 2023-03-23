c738009cc31f1adf5aa6e5ae26c84889b6dfb39e
Muhammad Iven Fesky <125557627+IvenFesky@users.noreply.github.com>
2023-03-23 13:32:21 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// Imports
+const express = require("express");
+const app = express();
+const port = 8000;
+
+// Static Files
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+// app.use("/img", express.static(__dirname + "public/img"));
+
+// Set Views
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("", (req, res) => {
+   res.render("index");
+});
+
+// Listen on port 3000
+app.listen(port, () => console.info(`Listening on port ${port}`));
